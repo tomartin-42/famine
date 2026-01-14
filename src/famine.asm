4a62eb6b6c4697bea5ee4c23acc42c90637b5805
tomartin <tomartin@student.42madrid.com>
2026-01-14 20:56:25 +0100

    init validate files types

--- src/famine.asm
+++ src/famine.asm
@@ -26,15 +26,24 @@ section .text
         test rax, rax
         jl .next_dir
         mov VAR(famine.fd_dir), rax
-        
+
         .dirent:
             mov rax, SC_GETDENTS
             mov rdi, VAR(famine.fd_dir)
-            lea rsi, famine.dirent_struc
+            lea rsi, VAR(famine.dirent_struc)
             mov rdx, 1024
-            syscall    
+            syscall
+            test rax, rax
+            jl .close_dir    
 
-    .next_dir:        ; fallo por rdi que lo cambia rdi y no apunta a la string de directorios       
+        .validate_files_types:
+
+        .close_dir:
+            mov rax, SC_CLOSE
+            mov rdi, VAR(famine.fd_dir)
+            syscall
+
+    .next_dir:        ; fallo por rdi que cambia y no apunta a la string de directorios 
         mov rcx, -1
         xor al, al
         repne scasb                 ;busca /0
